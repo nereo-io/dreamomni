dcfaeccaacfd53231dfe93fd26462d623a02adb7
刘苇 <liuweifly@yahoo.com>
2025-01-20 16:43:05 +0800

    更新orders 页面

--- app/[locale]/(default)/(console)/my-orders/page.tsx
+++ app/[locale]/(default)/(console)/my-orders/page.tsx
@@ -48,21 +48,21 @@ export default async function () {
     title: t("my_orders.title"),
     description: t("my_orders.description"),
     toolbar: {
-      items: [
-        {
-          title: t("my_orders.read_docs"),
-          icon: "RiBookLine",
-          url: "https://docs.shipany.ai",
-          target: "_blank",
-          variant: "outline",
-        },
-        {
-          title: t("my_orders.join_discord"),
-          icon: "RiDiscordFill",
-          url: "https://discord.gg/HQNnrzjZQS",
-          target: "_blank",
-        },
-      ],
+      // items: [
+      //   {
+      //     title: t("my_orders.read_docs"),
+      //     icon: "RiBookLine",
+      //     url: "https://docs.shipany.ai",
+      //     target: "_blank",
+      //     variant: "outline",
+      //   },
+      //   {
+      //     title: t("my_orders.join_discord"),
+      //     icon: "RiDiscordFill",
+      //     url: "https://discord.gg/HQNnrzjZQS",
+      //     target: "_blank",
+      //   },
+      // ],
     },
     columns: columns,
     data: orders,
