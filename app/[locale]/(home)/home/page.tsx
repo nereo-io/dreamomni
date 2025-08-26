ca1746e6c66b20fccd27ab8ef8a0df94b214fe8a
liuweifly <liuweifly@yahoo.com>
2025-08-26 20:52:03 +0800

    home 展示首页内容

--- app/[locale]/(home)/home/page.tsx
+++ app/[locale]/(home)/home/page.tsx
@@ -1,16 +1,56 @@
-import { useTranslations } from "next-intl"
+import FAQ from "@/components/blocks/faq";
+import Hero from "@/components/blocks/hero";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { ImageToVideoShowcase } from "@/components/blocks/image-to-video-showcase";
+import { AIModelsHero } from "@/components/blocks/ai-models-hero";
+import { AIVideoShowcase } from "@/components/blocks/ai-video-showcase";
+import CTA from "@/components/blocks/cta";
+import { getLandingPage } from "@/services/page";
 
-export default function HomePage() {
-  const t = useTranslations('pages.home')
-  
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  let canonicalUrl = `${process.env.NEXT_PUBLIC_WEB_URL}`;
+
+  if (locale !== "en") {
+    canonicalUrl = `${process.env.NEXT_PUBLIC_WEB_URL}/${locale}`;
+  }
+
+  return {
+    alternates: {
+      canonical: canonicalUrl,
+    },
+  };
+}
+
+export default async function LandingPage({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const page = await getLandingPage(locale);
+
   return (
-    <div className="text-center py-20">
-      <h1 className="text-4xl font-bold text-white mb-4">
-        {t('title')}
-      </h1>
-      <p className="text-gray-400 max-w-2xl mx-auto">
-        {t('description')}
-      </p>
-    </div>
-  )
+    <>
+      {/* {page.hero && <Hero hero={page.hero} />} */}
+      {/* <VideoFeatureShowcase data={videoFeatureShowcase} /> */}
+
+      {page.aiModelsHero && <AIModelsHero data={page.aiModelsHero} />}
+      {page.imageToVideoShowcase && (
+        <ImageToVideoShowcase data={page.imageToVideoShowcase} />
+      )}
+      {page.aiVideoShowcase && <AIVideoShowcase data={page.aiVideoShowcase} />}
+      {page.faq && <FAQ section={page.faq} />}
+      {page.cta && <CTA section={page.cta} />}
+      <div className="max-w-7xl mx-auto px-8 py-3">
+        <p className="text-sm text-muted-foreground text-center font-medium">
+          This platform is an independent product and is not affiliated with
+          Google.
+        </p>
+      </div>
+    </>
+  );
 }
