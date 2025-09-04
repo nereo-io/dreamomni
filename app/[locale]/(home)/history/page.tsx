ad7436d6a78a175ff808806590ae96ec9c581591
zhangchengming <zhangchengming@aishi.ai>
2025-09-04 12:00:26 +0800

    feat：历史记录移除黑色背景loading

--- app/[locale]/(home)/history/page.tsx
+++ app/[locale]/(home)/history/page.tsx
@@ -1,7 +1,5 @@
 import MyCreationsPage from "@/components/blocks/my-creations-page";
-import { Suspense } from "react";
 import { getTranslations } from "next-intl/server";
-import { Loader2 } from "lucide-react";
 
 export async function generateMetadata({
   params: { locale },
@@ -26,15 +24,5 @@ export async function generateMetadata({
 }
 
 export default function MyCreationsHistoryPage() {
-  return (
-    <Suspense
-      fallback={
-        <div className="flex justify-center items-center h-[calc(100vh-200px)]">
-          <Loader2 className="h-12 w-12 animate-spin text-blue-500" />
-        </div>
-      }
-    >
-      <MyCreationsPage />
-    </Suspense>
-  );
+  return <MyCreationsPage />;
 }
