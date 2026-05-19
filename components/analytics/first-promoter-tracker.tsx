4a9cf9de06e657216e67b65b0051ec16c565e882
Neyric <contact@neyric.dev>
2026-05-19 16:30:20 +0800

    Add FirstPromoter env vars and tracker
    (cherry picked from commit 37c68ee2801b204b83b71b64f8610d650a0ec358)

---
+++ components/analytics/first-promoter-tracker.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function FirstPromoterTracker() {
+  const accountId = process.env.NEXT_PUBLIC_FIRST_PROMOTER_ACCOUNT_ID;
+
+  if (!accountId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script id="first-promoter-init" strategy="afterInteractive">
+        {`
+          (function(w){w.fpr=w.fpr||function(){w.fpr.q=w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
+          fpr("init", { cid: "${accountId}" });
+          fpr("click");
+        `}
+      </Script>
+      <Script
+        id="first-promoter-fpr-js"
+        src="https://cdn.firstpromoter.com/fpr.js"
+        strategy="afterInteractive"
+      />
+    </>
+  );
+}
